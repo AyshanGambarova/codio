02bbdccb576abb4db94e3dfb97c30cea70abc352
User <User@WINDOWS-1TDC0VN>
2022-09-08 03:34:44 +0400

    task3 fixed

--- task3/js/main.js
+++ task3/js/main.js
@@ -6,22 +6,21 @@ let array = [
   { type: "C", value: "Ford" },
 ];
 
-const combinedItems = (arr = []) => {
-    const res = arr.reduce((acc, obj) => {
-       let found = false;
-       for (let i = 0; i < acc.length; i++) {
-          if (acc[i].type === obj.type) {
-             found = true;
-          };
-       }
-       if (!found) {
-          acc.push(obj);
-       }
-       return acc;
-    }, []);
-    return res;
- }
- console.log(combinedItems(array));
+let output = [];
 
+array.forEach(function (item) {
+  let existItem = output.filter(function (v, i) {
+    return v.type == item.type;
+  });
+  if (existItem.length) {
+    let existItemIndex = output.indexOf(existItem[0]);
+    output[existItemIndex].value = output[existItemIndex].value.concat(
+      item.value
+    );
+  } else {
+    if (typeof item.value == "string") item.value = [item.value];
+    output.push(item);
+  }
+});
 
- 
+console.log(output);
